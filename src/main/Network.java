1c33e85facff0f15e4b8bf89744a733941f7e1ad
Evanston <12412802@mail.sustech.edu.cn>
2025-05-11 21:40:08 +0800

    重构项目结构

---
+++ src/main/Network.java
@@ -0,0 +1,34 @@
+package src.main;
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoException;
+import com.mongodb.ServerApi;
+import com.mongodb.ServerApiVersion;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+public class Network {
+    public static void main(String[] args) {
+        String connectionString = "mongodb+srv://xiangyangyin45:<UXCA8jncOjl5JcKR>@cluster0.9spasgd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+        ServerApi serverApi = ServerApi.builder()
+                .version(ServerApiVersion.V1)
+                .build();
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(connectionString))
+                .serverApi(serverApi)
+                .build();
+        // Create a new client and connect to the server
+        try (MongoClient mongoClient = MongoClients.create(settings)) {
+            try {
+                // Send a ping to confirm a successful connection
+                MongoDatabase database = mongoClient.getDatabase("admin");
+                database.runCommand(new Document("ping", 1));
+                System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
+            } catch (MongoException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
